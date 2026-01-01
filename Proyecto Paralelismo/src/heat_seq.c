03dcd37de1309e9d02e9ce5023d31d5c3d0d4803
juansta02 <juansta02@gmail.com>
2026-01-01 21:50:13 +0100

    Update heat_seq.c

--- Proyecto Paralelismo/src/heat_seq.c
+++ Proyecto Paralelismo/src/heat_seq.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "grid.h"
 #include "config.h"
 
@@ -11,81 +12,41 @@ int heat_step(grid_t *g)
     int n_filas = g->ny;
 
     // reservar malla temporal
-    grid_t *g_copy;
-    if (grid_alloc(g_copy, n_cols, n_filas) != 0)
+    grid_t g_copy;
+    if (grid_alloc(&g_copy, n_cols, n_filas) != 0)
     {
         return -2;
     }
 
-    int max = n_cols * n_filas;
-    real_t *data = g->data;
-    for (int count = 0; count < max; count++)
+    size_t max = (size_t)n_cols * (size_t)n_filas;
+    memcpy(g_copy.data, g->data, max * sizeof(real_t));
+
+    // actualizar solo los puntos interiores 
+    for (int y = 1; y < n_filas - 1; ++y)
     {
-        int x = count % n_cols;
-        int y = count / n_cols;
+        for (int x = 1; x < n_cols - 1; ++x)
+        {
+            real_t up = GRID_AT(g, y - 1, x);
+            real_t down = GRID_AT(g, y + 1, x);
+            real_t left = GRID_AT(g, y, x - 1);
+            real_t right = GRID_AT(g, y, x + 1);
 
-        int n, sum;
-        int vals[4];
-
-        real_t mean;
-        ;
-        // operar para bordes
-        if (y > 0 && y < n_cols - 1 && x > 0 && x < n_filas - 1)
-        {
-            n = 0;
-            vals[n++] = GRID_AT(g, y - 1, x); // arriba
-            vals[n++] = GRID_AT(g, y + 1, x); // abajo
-            vals[n++] = GRID_AT(g, y, x - 1); // izquierda
-            vals[n++] = GRID_AT(g, y, x + 1); // derecha
-
+            GRID_AT(&g_copy, y, x) = (up + down + left + right) * (real_t)0.25;
         }
-        else if (y == 0) // si y == 0 -> borde superior
-        {
-            n = 0;
-            vals[n++] = GRID_AT(g, y + 1, x); // abajo
-            vals[n++] = GRID_AT(g, y, x - 1); // izquierda
-            vals[n++] = GRID_AT(g, y, x + 1); // derecha
-        }
-        else if (y == n_cols - 1) // si y == n_cols - 1 -> borde inferior
-        {
-            n = 0;
-            vals[n++] = GRID_AT(g, y - 1, x); // arriba
-            vals[n++] = GRID_AT(g, y, x - 1); // izquierda
-            vals[n++] = GRID_AT(g, y, x + 1); // derecha
-        }
-        else if (x == 0) // si x == 0 -> borde izquierdo
-        {
-            n = 0;
-            vals[n++] = GRID_AT(g, y - 1, x); // arriba
-            vals[n++] = GRID_AT(g, y + 1, x); // abajo
-            vals[n++] = GRID_AT(g, y, x + 1); // derecha
-        }
-        else if (x == n_filas - 1) // si x == n_filas - 1 -> borde derecho
-        {
-            n = 0;
-            vals[n++] = GRID_AT(g, y - 1, x); // arriba
-            vals[n++] = GRID_AT(g, y + 1, x); // abajo
-            vals[n++] = GRID_AT(g, y, x - 1); // izquierda
-        }
-
-        for (size_t i = 0; i < n; i++)
-        {
-            sum += vals[i];
-        }
-        mean = (real_t) sum / n; //cast to real_t so it gives a real_t result
-        GRID_AT(g_copy, y, x) = mean;
     }
 
     // copiar malla temporal a malla original
-    grid_swap(g, g_copy);
-    grid_free(g_copy);
+    grid_swap(g, &g_copy);
+    grid_free(&g_copy);
+    return 0;
 }
 
 int heat_iterate(grid_t *g, int nsteps)
 {
-    int check_res;
+    int check_res = 0;
     for (size_t i = 0; i < nsteps && check_res == 0; i++)
     {
         check_res = heat_step(g);
     }
+    return check_res;
 }
